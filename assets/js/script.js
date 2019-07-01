3bf22e83b2eeb24efefabb9d398c871f21a785fc
Yuji Ito <llamerada.jp@gmail.com>
2019-07-02 01:10:13 +0900

    add use case to side nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-    var sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav, {});
-
-    var dropdown = document.querySelectorAll('.dropdown-trigger')
-    M.Dropdown.init(dropdown, {});
+    M.AutoInit();
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown();
 });
